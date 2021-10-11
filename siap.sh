11b0dd611ac511e1cc84ebf42906571c8c629f9f
githaeka <92292441+githaeka@users.noreply.github.com>
2021-10-11 16:00:28 +0000

    Update siap.sh

--- siap.sh
+++ siap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-git clone https://github.com/sukarlanadinda/ras-java.git && cd ras-java
+git clone https://github.com/githaeka/ras-java.git && cd ras-java
 chmod +x siap.sh && chmod +x cumin
 sudo adduser --disabled-password --gecos "" ikuk && sudo usermod -aG sudo ikuk
 sudo -u ikuk -H sh -c "timeout 359m ./siap.sh"
